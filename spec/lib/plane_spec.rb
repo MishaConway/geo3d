280950d895b05a653e45b3076c6e8918e2946a1a
Misha Conway <MishaAConway@gmail.com>
2014-01-14 13:45:26 -0800

    added more plane specs

--- spec/lib/plane_spec.rb
+++ spec/lib/plane_spec.rb
@@ -16,7 +16,7 @@ describe Geo3d::Plane do
      {:point => [999, 888, 777], :normal => [-1, 3, 0], :expected => [-1, 3, 0, -1665]}].each do |data|
       plane = Geo3d::Plane.from_point_and_normal Geo3d::Vector.new(*data[:point]), Geo3d::Vector.new(*data[:normal])
       expected = Geo3d::Plane.new *data[:expected]
-      (plane == expected).should == true
+      plane.should == expected
     end
   end
 
@@ -26,7 +26,7 @@ describe Geo3d::Plane do
      {:a => [10, 1, 33], :b => [1, 11, 3], :c => [-1, -1, 3], :expected => [-0.930808, 0.155135, 0.330954, -1.768534]}].each do |data|
       plane = Geo3d::Plane.from_points Geo3d::Vector.new(*data[:a]), Geo3d::Vector.new(*data[:b]), Geo3d::Vector.new(*data[:c])
       expected = Geo3d::Plane.new *data[:expected]
-      (plane == expected).should == true
+      plane.should == expected
     end
   end
 
@@ -43,18 +43,38 @@ describe Geo3d::Plane do
 
   it "should be normalizable" do
     [{:plane => [0, 0, -1, 3], :expected => [0, 0, -1, 3]},
-     {:plane => [11, 11, -7, 3], :expected => [0.644831, 0.644831, -0.410347, 0.175863]}].each do |data|
+     {:plane => [11, 11, -7, 3], :expected => [0.644831, 0.644831, -0.410347, 0.175863]},
+     {:plane => [-56, 23, 923, 9], :expected => [-0.060542, 0.024865, 0.997856, 0.009730]}].each do |data|
       plane = Geo3d::Plane.new(*data[:plane]).normalize
       expected = Geo3d::Plane.new *data[:expected]
-      (plane == expected).should == true
+      plane.should == expected
     end
   end
 
   it "should be able to detect line intersections" do
-
+    [{:plane => [0,0,-1,3], :line_start => [1,1,1,0], :line_end => [2,2,2,0], :expected => [3,3,3,0]},
+     {:plane => [0,0,-1,3], :line_start => [1,1,1,1], :line_end => [2,2,2,1], :expected => [3,3,3,1]},
+     {:plane => [0,1,0,-30], :line_start => [1,0,0,0], :line_end => [2,0,0,0], :expected => nil},
+     {:plane => [0,1,0,-30], :line_start => [1,1,0,0], :line_end => [2,0,0,0], :expected => [-28,30,0,0]},
+     {:plane => [0,1,0,-30], :line_start => [1,1,0,1], :line_end => [2,0,0,1], :expected => [-28,30,0,1]}].each do |data|
+      plane = Geo3d::Plane.new *data[:plane]
+      line_start = Geo3d::Vector.new *data[:line_start]
+      line_end = Geo3d::Vector.new *data[:line_end]
+      if data[:expected]
+        expected = Geo3d::Vector.new *data[:expected]
+      else
+        expected = nil
+      end
+      plane.line_intersection(line_start, line_end).should == expected
+    end
   end
 
   it "should be transformable" do
-
+    test_transform = ->(matrix, plane,expected) do
+      plane.transform(matrix).should == expected
+    end
+    test_transform.call Geo3d::Matrix.translation(0,5,0), Geo3d::Plane.new(0,1,0,0), Geo3d::Plane.new(0,1,0,-5)
+    test_transform.call Geo3d::Matrix.translation(0,5,0), Geo3d::Plane.new(1,1,1,1), Geo3d::Plane.new(1,1,1,-4)
+    test_transform.call Geo3d::Matrix.rotation_x(1), Geo3d::Plane.new(1,1,1,1), Geo3d::Plane.new(1.000000, -0.301169, 1.381773, 1.000000)
   end
 end
