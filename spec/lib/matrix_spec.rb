77d2472826cdcb087f4ca920b5d645e450b01d68
Misha Conway <MishaAConway@gmail.com>
2014-01-14 18:42:00 -0800

    more matrix specs

--- spec/lib/matrix_spec.rb
+++ spec/lib/matrix_spec.rb
@@ -58,27 +58,62 @@ describe Geo3d::Plane do
   end
 
   it "should have a right handed perspective projection constructor" do
-
+    [{:fovy => 2, :aspect => 2, :zn => 2, :zf => 21000, :expected => [0.321046, 0.000000, 0.000000, 0.000000, 0.000000, 0.642093, 0.000000, 0.000000, 0.000000, 0.000000, -1.000095, -1.000000, 0.000000, 0.000000, -2.000190, 0.000000]}].each do |data|
+      matrix = Geo3d::Matrix.perspective_fov_rh data[:fovy], data[:aspect], data[:zn], data[:zf]
+      data[:expected].size.should == 16
+      expected = Geo3d::Matrix.new *data[:expected]
+      matrix.should == expected
+    end
   end
+
 
   it "should have a left handed perspective projection constructor" do
-
+    [{:fovy => 2, :aspect => 2, :zn => 2, :zf => 21000, :expected => [0.321046, 0.000000, 0.000000, 0.000000, 0.000000, 0.642093, 0.000000, 0.000000, 0.000000, 0.000000, 1.000095, 1.000000, 0.000000, 0.000000, -2.000190, 0.000000]}].each do |data|
+      matrix = Geo3d::Matrix.perspective_fov_lh data[:fovy], data[:aspect], data[:zn], data[:zf]
+      data[:expected].size.should == 16
+      expected = Geo3d::Matrix.new *data[:expected]
+      matrix.should == expected
+    end
   end
+
 
   it "should have a right handed orthographic projection constructor" do
-
+    [{:l => -100, :r => 100, :b => -200, :t => 200, :zn => 1, :zf => 2000, :expected => [0.010000, 0.000000, 0.000000, 0.000000, 0.000000, 0.005000, 0.000000, 0.000000, 0.000000, 0.000000, -0.000500, 0.000000, -0.000000, -0.000000, -0.000500, 1.000000]}].each do |data|
+      matrix = Geo3d::Matrix.ortho_off_center_rh data[:l], data[:r], data[:b], data[:t], data[:zn], data[:zf]
+      data[:expected].size.should == 16
+      expected = Geo3d::Matrix.new *data[:expected]
+      matrix.should == expected
+    end
   end
+
 
   it "should have a left handed orthographic projection constructor" do
-
+    [{:l => -100, :r => 100, :b => -200, :t => 200, :zn => 1, :zf => 2000, :expected => [0.010000, 0.000000, 0.000000, 0.000000, 0.000000, 0.005000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000500, 0.000000, -0.000000, -0.000000, -0.000500, 1.000000]}].each do |data|
+      matrix = Geo3d::Matrix.ortho_off_center_lh data[:l], data[:r], data[:b], data[:t], data[:zn], data[:zf]
+      data[:expected].size.should == 16
+      expected = Geo3d::Matrix.new *data[:expected]
+      matrix.should == expected
+    end
   end
+
 
   it "should have a right handed view constructor" do
-
+    [{:eye => [1, 2, 3], :focus => [200, 700, 88], :up => [0, 1, 0], :expected => [-0.392804, -0.878379, -0.272314, 0.000000, 0.000000, 0.296115, -0.955152, 0.000000, 0.919622, -0.375187, -0.116315, 0.000000, -2.366064, 1.411711, 2.531564, 1.000000]}].each do |data|
+      matrix = Geo3d::Matrix.look_at_rh Geo3d::Vector.new(*data[:eye]), Geo3d::Vector.new(*data[:focus]), Geo3d::Vector.new(*data[:up])
+      data[:expected].size.should == 16
+      expected = Geo3d::Matrix.new *data[:expected]
+      matrix.should == expected
+    end
   end
 
-  it "should have a left handed view constructor" do
 
+  it "should have a left handed view constructor" do
+    [{:eye => [1, 2, 3], :focus => [200, 700, 88], :up => [0, 1, 0], :expected => [0.392804, -0.878379, 0.272314, 0.000000, 0.000000, 0.296115, 0.955152, 0.000000, -0.919622, -0.375187, 0.116315, 0.000000, 2.366064, 1.411711, -2.531564, 1.000000]}].each do |data|
+      matrix = Geo3d::Matrix.look_at_lh Geo3d::Vector.new(*data[:eye]), Geo3d::Vector.new(*data[:focus]), Geo3d::Vector.new(*data[:up])
+      data[:expected].size.should == 16
+      expected = Geo3d::Matrix.new *data[:expected]
+      matrix.should == expected
+    end
   end
 
   it "should have a translation constructor" do
@@ -121,7 +156,7 @@ describe Geo3d::Plane do
   end
 
   it "should have an arbitrary axis rotation constructor" do
-    [{:axis => [1,1,0], :angle => 88.7, :expected => [0.870782,0.129218,-0.474385,0.000000,0.129218,0.870782,0.474385,0.000000,0.474385,-0.474385,0.741564,0.000000,0.000000,0.000000,0.000000,1.000000]}].each do |data|
+    [{:axis => [1, 1, 0], :angle => 88.7, :expected => [0.870782, 0.129218, -0.474385, 0.000000, 0.129218, 0.870782, 0.474385, 0.000000, 0.474385, -0.474385, 0.741564, 0.000000, 0.000000, 0.000000, 0.000000, 1.000000]}].each do |data|
       matrix = Geo3d::Matrix.rotation Geo3d::Vector.new(*data[:axis]), data[:angle]
       data[:expected].size.should == 16
       expected = Geo3d::Matrix.new *data[:expected]
@@ -130,7 +165,8 @@ describe Geo3d::Plane do
   end
 
   it "should have a reflection constructor" do
-    [{:plane => [1, 1, 1, 1], :expected => []}].each do |data|
+    [{:plane => [0, 1, 0, 0], :expected => [1.000000,0.000000,0.000000,0.000000,0.000000,-1.000000,0.000000,0.000000,0.000000,0.000000,1.000000,0.000000,0.000000,0.000000,0.000000,1.000000]},
+     {:plane => [0, 1, 0, 1], :expected => [1.000000,0.000000,0.000000,0.000000,0.000000,-1.000000,0.000000,0.000000,0.000000,0.000000,1.000000,0.000000,0.000000,-2.000000,0.000000,1.000000]}].each do |data|
       matrix = Geo3d::Matrix.reflection Geo3d::Plane.new(*data[:plane])
       data[:expected].size.should == 16
       expected = Geo3d::Matrix.new *data[:expected]
@@ -139,7 +175,7 @@ describe Geo3d::Plane do
   end
 
   it "should have a shadow constructor" do
-    [{:plane => [1, 1, 1, 1], :light_pos => [1, 1, 1, 1], :expected => []}].each do |data|
+    [{:plane => [0, 1, 0, 1], :light_pos => [0, 700, 0, 1], :expected => [701.000000,0.000000,0.000000,0.000000,0.000000,1.000000,0.000000,-1.000000,0.000000,0.000000,701.000000,0.000000,0.000000,-700.000000,0.000000,700.000000]}].each do |data|
       matrix = Geo3d::Matrix.shadow Geo3d::Vector.new(*data[:light_pos]), Geo3d::Plane.new(*data[:plane])
       data[:expected].size.should == 16
       expected = Geo3d::Matrix.new *data[:expected]
